ffc9e50a843612296541bb8cab8215ec7c9906df
Doosies <song961003@gmail.com>
2021-06-05 21:32:43 +0900

    #1904, 01타일

--- hi.js
+++ hi.js
@@ -1,26 +1,16 @@
 const stdin = (process.platform ==='linux'
 ? require('fs').readFileSync('dev/stdin').toString()
-: `2
-6
-12
-`).trim().split('\n');
+: `
+100000
+`
+).trim().split('\n');
 const input = (()=>{
     let line = 0;
     return ()=>stdin[line++].split(" ").map(Number);
 })();
 
-const len = input();
-const _case = Array.from(
-    {length:len},
-    ()=>input()
-);
-
-
-let arr = [1,1,1];
-for(i=3; i<Math.max(..._case); i++){
-    arr[i] = (arr[i-3] + arr[i-2]);
-}
-
-_case.map(val=>{
-    console.log(arr[val-1]);
-})
+const n = Number(input());
+let arr = [0,1,2];
+for(i=3; i<n+1; i++)
+    arr[i] = (arr[i-1] + arr[i-2])%15746;
+console.log(arr[n]);
