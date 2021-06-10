28b2cb79685e3e7f20cc1811c23b9dcf679e47c1
Doosies <song961003@gmail.com>
2021-06-10 19:56:01 +0900

    #1932, 정수 삼각형

--- hi.js
+++ hi.js
@@ -1,13 +1,9 @@
 const stdin = (process.platform ==='linux'
 ? require('fs').readFileSync('dev/stdin').toString()
 : `
-6
-0 1 2 3 4 5
-1 0 2 3 4 5
-1 2 0 3 4 5
-1 2 3 0 4 5
-1 2 3 4 0 5
-1 2 3 4 5 0
+2
+0
+1 1
 `// 정답: 3058
 ).trim().split('\n');
 const input = (()=>{
@@ -15,37 +11,13 @@ const input = (()=>{
     return ()=>stdin[line++].split(" ").map(Number);
 })();
 
-const dfs = (L, K) => {
-    if (L === N / 2) {
-      const sTeam = [], lTeam = [];
-      let sSum = 0, lSum = 0;
-  
-      for (let i = 0; i < N; i++) {
-        if (check[i]) sTeam.push(i);
-        else lTeam.push(i);
-      }
-      for (let i = 0; i < N / 2; i++) {
-        for (let j = i + 1; j < N / 2; j++) {
-          sSum += graph[sTeam[i]][sTeam[j]] + graph[sTeam[j]][sTeam[i]];
-          lSum += graph[lTeam[i]][lTeam[j]] + graph[lTeam[j]][lTeam[i]];
-        }
-      }
-      min = Math.min(min, Math.abs(sSum - lSum));
-      return;
+const n = input();
+const trg = Array.from({length:n}, ()=> input());
+for(let i = n-2; i >= 0; i--){
+    for(let j = 0; j < trg[i].length; j++){
+        const l = trg[i+1][j] + trg[i][j];
+        const r = trg[i+1][j+1] + trg[i][j];
+        trg[i][j] = Math.max(l,r);
     }
-  
-    for (let i = K; i < N; i++) {
-      check[i] = 1;
-      dfs(L + 1, i + 1);
-      check[i] = 0;
-    }
-  }
-  
-  const N = parseInt(input());
-  const graph = Array.from({length:N}, () => input());
-  const check = Array.from({length:N}, () => 0);
-  
-  let min = Number.MAX_SAFE_INTEGER;
-  
-  dfs(0, 0);
-  console.log(min);
+}
+console.log(parseInt(trg[0]));
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,13 +1,2 @@
- playerCombination){
-//     const teamA = teams[0];
-//     const teamB = teams[1];
-//     let sum1 = 0;
-//     let sum2 = 0;
-//     for(let i = 0; i < n/2; i++){
-//         for(let j = i+1; j < n/2; j++){
-//             sum1 += statusArray[teamA[i]-1][teamA[j]-1] + statusArray[teamA[j]-1][teamA[i]-1];
-//             sum2 += statusArray[teamB[i]-1][teamB[j]-1] + statusArray[teamB[j]-1][teamB[i]-1];
-//         }
-//     }
-//     result = Math.min(result, Math.abs(sum1 - sum2));
-// }
+
+        max = Math.max(l,r);
