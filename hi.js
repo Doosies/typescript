299bbf94cc42a56c2408f1784d7594b2ceba3e6c
Doosies <song961003@gmail.com>
2021-06-10 19:30:58 +0900

    #14889, 스타트와 링크

--- hi.js
+++ hi.js
@@ -1,17 +1,13 @@
 const stdin = (process.platform ==='linux'
 ? require('fs').readFileSync('dev/stdin').toString()
 : `
-10
-711 572 325
-209 187 673
-512 930 898
-759 85 260
-136 226 532
-201 3 959
-132 607 359
-601 775 848
-462 776 920
-74 807 671
+6
+0 1 2 3 4 5
+1 0 2 3 4 5
+1 2 0 3 4 5
+1 2 3 0 4 5
+1 2 3 4 0 5
+1 2 3 4 5 0
 `// 정답: 3058
 ).trim().split('\n');
 const input = (()=>{
@@ -19,12 +15,37 @@ const input = (()=>{
     return ()=>stdin[line++].split(" ").map(Number);
 })();
 
-let n = Number(input());
-let color = Array.from({length:n}, ()=> input());
-
-for(let i=1; i<color.length; i++){
-    color[i][0] = Math.min(color[i-1][1], color[i-1][2]) + color[i][0];
-    color[i][1] = Math.min(color[i-1][0], color[i-1][2]) + color[i][1];
-    color[i][2] = Math.min(color[i-1][0], color[i-1][1]) + color[i][2];
-}
-console.log(Math.min(...color[n-1]));
+const dfs = (L, K) => {
+    if (L === N / 2) {
+      const sTeam = [], lTeam = [];
+      let sSum = 0, lSum = 0;
+  
+      for (let i = 0; i < N; i++) {
+        if (check[i]) sTeam.push(i);
+        else lTeam.push(i);
+      }
+      for (let i = 0; i < N / 2; i++) {
+        for (let j = i + 1; j < N / 2; j++) {
+          sSum += graph[sTeam[i]][sTeam[j]] + graph[sTeam[j]][sTeam[i]];
+          lSum += graph[lTeam[i]][lTeam[j]] + graph[lTeam[j]][lTeam[i]];
+        }
+      }
+      min = Math.min(min, Math.abs(sSum - lSum));
+      return;
+    }
+  
+    for (let i = K; i < N; i++) {
+      check[i] = 1;
+      dfs(L + 1, i + 1);
+      check[i] = 0;
+    }
+  }
+  
+  const N = parseInt(input());
+  const graph = Array.from({length:N}, () => input());
+  const check = Array.from({length:N}, () => 0);
+  
+  let min = Number.MAX_SAFE_INTEGER;
+  
+  dfs(0, 0);
+  console.log(min);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,30 +1,13 @@
-const stdin = (process.platform ==='linux'
-? require('fs').readFileSync('dev/stdin').toString()
-: `
-10
-711 572 325
-209 187 673
-512 930 898
-759 85 260
-136 226 532
-201 3 959
-132 607 359
-601 775 848
-462 776 920
-74 807 671
-`// 정답: 3058
-).trim().split('\n');
-const input = (()=>{
-    let line = 0;
-    return ()=>stdin[line++].split(" ").map(Number);
-})();
-
-let n = Number(input());
-let color = Array.from({length:n}, ()=> input());
-
-for(let i=1; i<color.length; i++){
-    color[i][0] = Math.min(color[i-1][1], color[i-1][2]) + color[i][0];
-    color[i][1] = Math.min(color[i-1][0], color[i-1][2]) + color[i][1];
-    color[i][2] = Math.min(color[i-1][0], color[i-1][1]) + color[i][2];
-}
-console.log(Math.min(...color[n-1]));
+ playerCombination){
+//     const teamA = teams[0];
+//     const teamB = teams[1];
+//     let sum1 = 0;
+//     let sum2 = 0;
+//     for(let i = 0; i < n/2; i++){
+//         for(let j = i+1; j < n/2; j++){
+//             sum1 += statusArray[teamA[i]-1][teamA[j]-1] + statusArray[teamA[j]-1][teamA[i]-1];
+//             sum2 += statusArray[teamB[i]-1][teamB[j]-1] + statusArray[teamB[j]-1][teamB[i]-1];
+//         }
+//     }
+//     result = Math.min(result, Math.abs(sum1 - sum2));
+// }
