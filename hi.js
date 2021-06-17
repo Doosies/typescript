6c7a486d9bbd3bab61429fce2ade213892c72e3c
Doosies <song961003@gmail.com>
2021-06-17 18:27:23 +0900

    #카카오 코테 2020, 자물쇠와 열쇠

--- hi.js
+++ hi.js
@@ -10,56 +10,98 @@
 //     return ()=>stdin[line++].split(" ");
 // })();
 
-const isCorrectChar = (str) =>{
-    let cnt = 0;
-    for(c of str){
-        if( c === '(' ) cnt ++;
-        else cnt --;
-        if( cnt < 0) return false;
-    }
-    return cnt === 0 ? true : false;
+
+const getJustData = (data, keyVal) =>{
+    const justData = [];
+    data.map((row, y) => row.map((col, x) =>{
+        if( col === keyVal) justData.push([y,x]);
+    }));
+    return justData;
 }
-const isBalancedChar = (str) =>{
-    let cnt = 0;
-    for(c of str){
-        if( c === '(' ) cnt ++;
-        else cnt --;
-    }
-    return cnt === 0 ? true : false;
-}
-const getUV = (str) =>{
-    let stackStr = '';
-    for( let i=0; i<str.length; i++){
-        stackStr += str[i];
-        if( isBalancedChar(stackStr)){
-            return [stackStr, str.slice(i+1, str.length)];
+const rotate = (data) =>{
+    const last = data.length;
+    const result = [];
+    for(let i=0; i<last; i++){
+        const tmpRow = [];
+        for(let j = last - 1; j >= 0; j--){
+            tmpRow.push(data[j][i]);
         }
+        result.push(tmpRow);
     }
+    return result;
 }
-const getReverse = (str) =>{
-    let reversed = '';
-    for(let c of str){
-        if( c === '(') reversed += ')';
-        else reversed += '(';
-    }
-    return reversed;
-}
-const solution = (p) =>{
-    if( p == '' ) return '';
-    let [u, v] = getUV(p);
-    // 문자열 u가 올바른 괄호 문자열이라면
-    if( isCorrectChar(u) ){
-        return u + solution(v);
-    // 문자열 u가 올바른 괄호 문자열이 아니라면
-    }else{
-        let str = '(';
-        str += solution(v);
-        str += ')';
-        str += getReverse(u.slice(1,u.length-1));
-        return str;
-    }
+const getRotatedData = (originalData) =>{
+    const original = originalData;
+    const flipLeft = rotate(original);
+    const upSideDown = rotate(flipLeft);
+    const flipDouble = rotate(upSideDown);
+
+    return [
+        getJustData(original,1),
+        getJustData(flipLeft, 1), 
+        getJustData(upSideDown, 1), 
+        getJustData(flipDouble, 1)
+    ];
 }
 
-const str = '()))((()';
-const s = solution(str);
+const solution = (key, lock) =>{
+    const lockSize = lock.length;
+    const keySize = key.length;
+
+    const justLock = getJustData(lock, 0);
+    const rotatedJustKey = getRotatedData(key);
+    const lockNum = justLock.length;
+
+    const checkDuple = {};
+    justLock.forEach( v =>{
+        checkDuple[v] = true;
+    });
+
+    for( let ym = 0; ym < lockSize; ym++){
+        for( let xm = 0; xm < lockSize; xm++){
+            for( let keyIdx = 0; keyIdx<4; keyIdx++){
+                const shape = rotatedJustKey[keyIdx];
+                const added = Array.from({length:4}, ()=>[]);
+                shape.map(([y,x])=>{
+                    if( y+ym < lockSize && x+xm < lockSize)
+                        added[0].push([y+ym, x+xm]);
+                    if( y+ym < lockSize && x-xm > -1)
+                        added[1].push([y+ym, x-xm]);
+                    if( y-ym > -1 && x+xm < lockSize)
+                        added[2].push([y-ym, x+xm]);
+                    if( y-ym > -1 && x-xm > -1)
+                        added[3].push([y-ym, x-xm]);
+                });
+                for(let i=0; i<4; i++){
+                    if(added[i].length === lockNum){
+                        const isSame = added[i].every( val => checkDuple[val]);
+                        if( isSame ){
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
+
+const key = [
+    [0, 0, 1], 
+    [0, 1, 0], 
+    [0, 1, 0]
+];
+const lock = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+    [1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+const s = solution(key, lock);
 console.log(s);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,12 +1,31 @@
-const solution = (p) =>{
-//     if( p == '' ) return '';
-//     let [u, v] = getUV(p);
-//     // 문자열 u가 올바른 괄호 문자열이라면
-//     if( isBalancedChar(u){
-//         return u + solution(p);
-//     // 문자열 u가 올바른 괄호 문자열이 아니라면
-//     }else{
-//         let str = '(';
+ = {};
+    justLock.forEach( v =>{
+        checkDuple[v] = true;
+    });
 
-//     }
-// }
+    for( let ym = 0; ym < lockSize; ym++){
+        for( let xm = 0; xm < lockSize; xm++){
+            for( let keyIdx = 0; keyIdx<4; keyIdx++){
+                const shape = rotatedJustKey[keyIdx];
+                const added = Array.from({length:4}, ()=>[]);
+                shape.map(([y,x])=>{
+                    if( y+ym < lockSize && x+xm < lockSize)
+                        added[0].push([y+ym, x+xm]);
+                    if( y+ym < lockSize && x-xm > -1)
+                        added[1].push([y+ym, x-xm]);
+                    if( y-ym > -1 && x+xm < lockSize)
+                        added[2].push([y-ym, x+xm]);
+                    if( y-ym > -1 && x-xm > -1)
+                        added[3].push([y-ym, x-xm]);
+                });
+                for(let i=0; i<4; i++){
+                    if(added[i].length === lockNum){
+                        const isSame = added[i].every( val => checkDuple[val]);
+                        if( isSame ){
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
