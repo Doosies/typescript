fce30abe12406f5c2e6a67666f8e18884f148d11
Doosies <song961003@gmail.com>
2021-06-19 17:59:28 +0900

    #11053, 가장 긴 증가하는 부분 수열

--- hi.js
+++ hi.js
@@ -1,23 +1,43 @@
 const stdin = (process.platform ==='linux'
 ? require('fs').readFileSync('dev/stdin').toString()
 : `
-2
-`// 정답: 75
+13
+1 5 10 10 5 3 13 18 15 16 17
+`
 ).trim().split('\n');
 const input = (()=>{
     let line = 0;
     return ()=>stdin[line++].split(" ");
 })();
 
+
 const n = Number(input());
-const arr = [0,1,1,1,1,1,1,1,1,1];
-for( let i=1; i<n; i++){
-    const tmp = [...arr];
-    for(let j=0; j<10; j++){
-        const left = tmp[j-1] ? tmp[j-1] : 0;
-        const right = tmp[j+1] ? tmp[j+1] : 0;
-        arr[j] = (left+right)%1000000000;
+const arr = input().map(v => Number(v));
+const dp = [];
+
+const insertSort = (dpList) =>{
+    const len = dp.length;
+    const [_, cnt] = dpList;
+    for(let i=0; i<len; i++){
+        if( dp[i][1] <= cnt){
+            dp.splice(i,0,dpList);
+            return;
+        }
     }
+    dp.push(dpList);
 }
-const result = arr.reduce((sum, curr)=>sum+=curr);
-console.log(result%1000000000);
+const findDP = (num) =>{
+    const len = dp.length;
+    for(let i=0; i<len; i++){
+        if( dp[i][0] < num){
+            return [num, dp[i][1]+1];
+        }
+    }
+    return [num, 1];
+}
+
+for( let i=0; i<n; i++){
+    let tmpDp = findDP(arr[i]);
+    insertSort(tmpDp);
+}
+console.log(dp[0][1]);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,34 @@
-1000000000
+
+const insertSort = (dpList) =>{
+    const len = dp.length;
+    const [num, _] = dpList;
+    for(let i=0; i<len; i++){
+        if( dp[i][0] <= num){
+            dp.splice(i,0,dpList);
+            continue;
+        }
+    }
+    dp.push(dpList);
+}
+const findDP = (num) =>{
+    const len = dp.length;
+    for(let i=0; i<len; i++){
+        if( dp[i][0] < num)
+            return [num, dp[i][1]+1];
+    }
+    return [num, 1];
+}
+
+for( let i=0; i<n; i++){
+    let tmpDp = [];
+    if( arr[i] !== arr[i-1]){
+        tmpDp = findDP(arr[i]);
+    }
+    else{
+        tmpDp = [arr[i], dp[i-1][1]];
+    }
+    insertSort(tmpDp);
+    result = Math.max(result, tmpDp[1]);
+}
+// console.log(dp);
+console.log(result);
