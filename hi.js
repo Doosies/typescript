79b91a2f7d0209e5d5cf6808db3e1e4e92fc87ec
Doosies <song961003@gmail.com>
2021-06-24 10:22:03 +0900

    #11722, 가장 긴 감소하는 부분 수열

--- hi.js
+++ hi.js
@@ -1,8 +1,8 @@
 const stdin = (process.platform ==='linux'
 ? require('fs').readFileSync('dev/stdin').toString()
 : `
-13
-1 5 10 10 5 3 13 18 15 16 17
+4
+2 4 3 1
 `
 ).trim().split('\n');
 const input = (()=>{
@@ -14,22 +14,26 @@ const input = (()=>{
 const n = Number(input());
 const arr = input().map(v => Number(v));
 const dp = [];
+let maxDp = -1;
 
-const insertSort = (dpList) =>{
+const insertSort = (inputList) =>{
     const len = dp.length;
-    const [_, cnt] = dpList;
+    const [inputNum, inputDp] = inputList;
     for(let i=0; i<len; i++){
-        if( dp[i][1] <= cnt){
-            dp.splice(i,0,dpList);
+
+        if( dp[i][1] <= inputDp){
+            dp.splice(i,0,inputList);
             return;
         }
     }
-    dp.push(dpList);
+    dp.push(inputList);
 }
 const findDP = (num) =>{
     const len = dp.length;
     for(let i=0; i<len; i++){
-        if( dp[i][0] < num){
+        // dp의 숫자가 num보다 크다면
+        if( dp[i][0] > num){
+            // dp에 1을 더해서 리턴
             return [num, dp[i][1]+1];
         }
     }
@@ -37,7 +41,10 @@ const findDP = (num) =>{
 }
 
 for( let i=0; i<n; i++){
+    // 현재 숫자의 dp를 얻어옴
     let tmpDp = findDP(arr[i]);
     insertSort(tmpDp);
+    // console.log("!!");
 }
+console.log(dp);
 console.log(dp[0][1]);
