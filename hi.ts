8e74ce02170653f0825b104a813b912d1bac09dd
Doosies <song961003@gmail.com>
2021-07-06 12:39:46 +0900

    #1912, 연속합

--- hi.ts
+++ hi.ts
@@ -1,43 +1,21 @@
-type inputType = () => string[];
-
 const stdin: string = (process.platform ==='linux'
 ? require('fs').readFileSync('dev/stdin').toString()
 : `
-AABBAA
-BA
+15
+10 -4 3 1 5 6 -35 35 1 -5 -6 12 -20 -30 500
+
 `).trim().split('\n');
-const input: inputType = (()=>{
+const input: () => string[] = (()=>{
     let line = 0;
-    return () => stdin[line++].toString().split("");
+    return () => stdin[line++].toString().split(" ");
 })();
 
+// const t = input();
+const n: number = Number(input());
+const arr: number[] = input().map((v: string) => +v);
+const dp: number [] = [arr[0]];
 
-let longStr: string[] = input();
-longStr.splice(0,0,"0");
-let shortStr: string[] = input();
-shortStr.splice(0,0,"0");
-if (longStr.length < shortStr.length) {
-    [longStr,shortStr] = [shortStr,longStr];
+for (let i=1; i<n; i++) {
+    dp[i] = Math.max(arr[i], dp[i-1] + arr[i]);
 }
-const endI = longStr.length;
-const endJ = longStr.length;
-const LCS: number[][] = Array.from({length:endI}, () => []);
-
-for (let i=0; i<endI; i++) {
-    for (let j=0; j<endJ; j++) {
-        if (i === 0 || j === 0) {
-            LCS[i][j] = 0;
-        }
-        else if (longStr[i] === shortStr[j]) {
-            LCS[i][j] = LCS[i-1][j-1] + 1;
-        }
-        else {
-            LCS[i][j] = Math.max(LCS[i][j-1], LCS[i-1][j]);
-        }
-    }
-}
-// const maxTmp = LCS.map((row: number[]) => Math.max(...row));
-// const max = Math.max(...maxTmp);
-// console.log(LCS.join("\n"));
-// console.log(LCS);
-console.log(LCS[endI-1][endJ-1]);
+console.log(Math.max(...dp));
