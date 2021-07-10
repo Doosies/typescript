8f023b0e373fff8a359e25fe5c92fb7cb3441a53
Doosies <song961003@gmail.com>
2021-07-10 15:55:05 +0900

    #3036, 링

--- hi.ts
+++ hi.ts
@@ -1,48 +1,22 @@
 const stdin: string = (process.platform ==='linux'
 ? require('fs').readFileSync(0, 'utf-8')
 : `
-3
-6
-34
-38
+4
+300 1 1 300
 `).trim().split('\n');
 const input = (()=>{
     let line = 0;
     return ()=>stdin[line++].split(" ").map((v:string) => +v);
 })();
 
-function GCDs(num: number[]): number {
-    const len = num.length;
-
-    for (let i=0; i<len-1; i++) {
-        let a = num[i];
-        let b = num[i+1];
-        while (b) {
-            [a, b] = [b, a%b];
-        }
-        num[i+1] = a;
-    }
-    return num[len-1];
-}
-function commonFactor(n: number): number[] {
-    const arr: number[] = [];
-
-    for (let i=1; i<=n**0.5; i++) {
-        if (i * i === n) arr.push(i);
-        else if (n % i === 0) arr.push( i, n/i);
-    }
-    if (arr[0] === 1) arr.splice(0,1);
-    return arr;
+function gcd(a: number, b: number): number {
+    return b ? gcd(b, a%b) : a;
 }
 
 const N: number = +input();
-const arr: number[] = Array.from({length:N}, ()=>+input());
-const rstArr: number[] = [];
-arr.sort((a,b) => a-b);
+const arr: number[] = input();
 
 for (let i=1; i<N; i++) {
-    rstArr[i-1] = arr[i] - arr[i-1];
+    const g = gcd(arr[0], arr[i]);
+    console.log(`${arr[0]/g}/${arr[i]/g}`);
 }
-const result = commonFactor(GCDs(rstArr));
-result.sort((a,b) => a-b);
-console.log(result.join(" "));
