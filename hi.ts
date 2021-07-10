744d70638faef4308710a300c4ceaa2bdaa1f15a
Doosies <song961003@gmail.com>
2021-07-10 15:26:12 +0900

    #2981, 검문

--- hi.ts
+++ hi.ts
@@ -1,35 +1,48 @@
-// // // readline 사용
-// // const stdin: string[][] = [];
-// // const rl = require('readline').createInterface({
-// //   input: process.stdin,
-// //   output: process.stdout
-// // });
-
-// // rl.on('line', function (line: string[]): void {
-// //     stdin.push(line)
-// // }).on('close', function (): never {
-// //     solution();
-// //     process.exit();
-// // });
-
-// // // fs 사용
 const stdin: string = (process.platform ==='linux'
 ? require('fs').readFileSync(0, 'utf-8')
 : `
-24 18
+3
+6
+34
+38
 `).trim().split('\n');
 const input = (()=>{
     let line = 0;
     return ()=>stdin[line++].split(" ").map((v:string) => +v);
 })();
 
-function GCD(a: number, b: number): number {
-    return b ? GCD(b, a%b) : a;
+function GCDs(num: number[]): number {
+    const len = num.length;
+
+    for (let i=0; i<len-1; i++) {
+        let a = num[i];
+        let b = num[i+1];
+        while (b) {
+            [a, b] = [b, a%b];
+        }
+        num[i+1] = a;
+    }
+    return num[len-1];
+}
+function commonFactor(n: number): number[] {
+    const arr: number[] = [];
+
+    for (let i=1; i<=n**0.5; i++) {
+        if (i * i === n) arr.push(i);
+        else if (n % i === 0) arr.push( i, n/i);
+    }
+    if (arr[0] === 1) arr.splice(0,1);
+    return arr;
 }
 
-// const N: number = +input();
-const [l,r]: number[] = input();
-const gcd: number = GCD(l,r);
+const N: number = +input();
+const arr: number[] = Array.from({length:N}, ()=>+input());
+const rstArr: number[] = [];
+arr.sort((a,b) => a-b);
 
-console.log(gcd);
-console.log(gcd * (l/gcd) * (r/gcd));
+for (let i=1; i<N; i++) {
+    rstArr[i-1] = arr[i] - arr[i-1];
+}
+const result = commonFactor(GCDs(rstArr));
+result.sort((a,b) => a-b);
+console.log(result.join(" "));
