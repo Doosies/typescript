b915ebceaa91b42c59377db5155bbdf71dd2f7c2
Doosies <song961003@gmail.com>
2021-07-29 18:58:35 +0900

    #11051, 이항계수2

--- hi.ts
+++ hi.ts
@@ -1,22 +1,21 @@
 const stdin: string = (process.platform ==='linux'
 ? require('fs').readFileSync(0, 'utf-8')
 : `
-4
-300 1 1 300
+55 8
 `).trim().split('\n');
 const input = (()=>{
     let line = 0;
     return ()=>stdin[line++].split(" ").map((v:string) => +v);
 })();
 
-function gcd(a: number, b: number): number {
-    return b ? gcd(b, a%b) : a;
+const [n, k]: number[] = input();
+let result: number[][] = Array.from({length:n+1}, ()=>[1]);
+
+for (let i=0; i<= n; i++) {
+    for (let j=0; j<=i; j++) {
+        if (j===0 || j===i) result[i][j] = 1;
+        else result[i][j] = (result[i-1][j-1]% 10007 + result[i-1][j]% 10007)%10007;
+    }
 }
 
-const N: number = +input();
-const arr: number[] = input();
-
-for (let i=1; i<N; i++) {
-    const g = gcd(arr[0], arr[i]);
-    console.log(`${arr[0]/g}/${arr[i]/g}`);
-}
+console.log(result[n][k] % 10007);
