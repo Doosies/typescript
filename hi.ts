9b04ac186e9da40884971b5ea886eb4d4731b756
Doosies <song961003@gmail.com>
2021-09-09 18:08:28 +0900

    #10773, 제로

--- hi.ts
+++ hi.ts
@@ -1,48 +1,20 @@
 const stdin: string = (process.platform ==='linux'
 ? require('fs').readFileSync(0, 'utf-8')
 : `
-14
-push 1
-push 2
-top
-size
-empty
-pop
-pop
-pop
-size
-empty
-pop
-push 3
-empty
-top
+
 `).trim().split('\n');
 const input = (()=>{
     let line = 0;
-    return ()=> stdin[line++].split(" ");//.map( v => +v);
+    return ()=> stdin[line++].split(" ").map( v => +v);
 })();
 
 const C = +input();
-const stack: string[] = [];
-let result = '';
-let size = 0;
-
-const cmdObj = {
-    push: (n: string) => {stack[size]=n; size++;},
-    pop: () => {
-        if (size) {
-            size --;
-            return stack.splice(-1).toString();
-        }else return -1;
-    },
-    size: () => size,
-    empty: () => !size ? 1 : 0,
-    top: () => size ? stack[size-1] : -1,
-};
+const stack: number[] = [];
 
 for (let i=0; i<C; i++) {
-    const [cmd, num] = input();
-    if (cmd === "push") cmdObj.push(num);
-    else result += `${(cmdObj[cmd]())}\n`;
+    const num = +input();
+    if (num === 0) stack.pop();
+    else stack.push(num);
 }
+const result = stack.reduce((acc,curr)=>acc+curr,0);
 console.log(result);
