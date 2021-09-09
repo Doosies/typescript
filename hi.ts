1d7c92e7504705ba36ab7a2ac19e346e9b372c68
Doosies <song961003@gmail.com>
2021-09-09 18:00:37 +0900

    #10828, 스택

--- hi.ts
+++ hi.ts
@@ -1,33 +1,48 @@
 const stdin: string = (process.platform ==='linux'
 ? require('fs').readFileSync(0, 'utf-8')
 : `
-25 12
+14
+push 1
+push 2
+top
+size
+empty
+pop
+pop
+pop
+size
+empty
+pop
+push 3
+empty
+top
 `).trim().split('\n');
 const input = (()=>{
     let line = 0;
-    return ()=> stdin[line++].split(" ").map( v => +v);
+    return ()=> stdin[line++].split(" ");//.map( v => +v);
 })();
 
-// 2와 5의 갯수를 구함
-// 여기서 n은 n!을 의미함
-function calc(n: number) {
-    let five = 0;
-    let two = 0;
+const C = +input();
+const stack: string[] = [];
+let result = '';
+let size = 0;
 
-    for (let i = 2; i <= n; i *= 2) 
-        two += Math.floor(n / i);
-    for (let i = 5; i <= n; i *= 5)
-        five += Math.floor(n / i);
+const cmdObj = {
+    push: (n: string) => {stack[size]=n; size++;},
+    pop: () => {
+        if (size) {
+            size --;
+            return stack.splice(-1).toString();
+        }else return -1;
+    },
+    size: () => size,
+    empty: () => !size ? 1 : 0,
+    top: () => size ? stack[size-1] : -1,
+};
 
-    return [two, five];
+for (let i=0; i<C; i++) {
+    const [cmd, num] = input();
+    if (cmd === "push") cmdObj.push(num);
+    else result += `${(cmdObj[cmd]())}\n`;
 }
-
-const [M, N] = input();
-const m = calc(M);
-const n = calc(N);
-const mn = calc(M-N);
-
-const two = m[0] - (n[0] + mn[0]);
-const five = m[1] - (n[1] + mn[1]);
-
-console.log(Math.min(two, five));
+console.log(result);
